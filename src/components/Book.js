810fe193e65d281432c854c2a24a3c8a1d6f6c9c
Bianca Rosca <rosca.bianca@yahoo.com>
2021-01-26 22:08:17 +0200

    browse component

---
+++ src/components/Book.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Book = () => {
+
+    return(
+        <div>
+            <h2>10.99$</h2>
+            <h3>A journey thorugh our body</h3>
+            <h4>Martha Goodwill</h4>
+            <div>reviews</div>
+        </div>
+    );
+}
+
+export default Book;
--- src/pages/BrowsePage.js
+++ src/pages/BrowsePage.js
@@ -2,15 +2,27 @@ import React from 'react';
 import Header from '../components/Header';
 import SideNav from '../components/SideNav';
 import Footer from '../components/Footer';
+import BrowseComponent from '../components/BrowseComponent';
+import styled from 'styled-components';
 
 const BrowsePage = () => {
     return(
         <>
             <Header />
-            <SideNav />
+            <StyledWrapper>
+                <SideNav />
+                <BrowseComponent />
+            </StyledWrapper>
             <Footer />
         </>
     );
 }
+
+const StyledWrapper = styled.div`
+    height: 90vh;
+    width: 100%;
+    display: flex;
+
+`
 
 export default BrowsePage;
