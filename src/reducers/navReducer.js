9145b6712970295a672ae302a2f0fbc4abbe06bc
Bianca Rosca <rosca.bianca@yahoo.com>
2021-02-02 17:51:32 +0200

    nav active items

--- src/reducers/navReducer.js
+++ src/reducers/navReducer.js
@@ -1,10 +1,12 @@
 
 const initialState = {
-    'browse': true,
+    'browse': false,
     'wishlist' : false,
     'cart': false,
     'history' : false,
 }
+const path = window.location.pathname;
+initialState[path.slice(1)] = true;
 
 export const navItemsState = (state=initialState,action) => {
     switch(action.type){
